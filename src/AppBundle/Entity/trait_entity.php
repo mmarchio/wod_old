0d116db3b54a2bd06c185ac4378184e02179aacf
mmarchio <mmarchio@users.noreply.github.com>
2021-02-10 15:32:18 -0800

    implement JsonSerializable

--- src/AppBundle/Entity/trait_entity.php
+++ src/AppBundle/Entity/trait_entity.php
@@ -10,8 +10,17 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="trait_entity")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\trait_entityRepository")
  */
-class trait_entity
+class trait_entity implements JsonSerializable
 {
+    public function jsonSerialize(): string
+    {
+        $a = [];
+        foreach ($this as $k => $v) {
+            $a[$k] = $v;
+        }
+        return $a;
+    }
+
     /**
      * @var int
      *
